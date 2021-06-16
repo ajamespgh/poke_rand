5902fd00ff115aa7673d388f66fddcc730b984a3
Alex James <jalex199345@gmail.com>
2021-06-15 21:02:48 -0400

    added dex entries and minor style changes

--- poke_rand.py
+++ poke_rand.py
@@ -18,6 +18,11 @@ def poke_search():
 @app.route("/random")
 def poke_rand():
     rand_id = str(random.randint(0, 899))
-    r = requests.get(f'https://pokeapi.co/api/v2/pokemon/{rand_id}')
-    rand_poke = r.json()
-    return render_template('poke.html.jinja', nav="random", title="Random", poke=rand_poke)
+    rand_poke = requests.get(f'https://pokeapi.co/api/v2/pokemon/{rand_id}').json()
+    rand_poke_info = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{rand_id}/').json()
+    dex_entry = ''
+    for entry in rand_poke_info['flavor_text_entries']:
+        if entry['language']['name'] == 'en':
+            dex_entry = entry['flavor_text']
+            break
+    return render_template('poke.html.jinja', nav="random", title="Random", poke=rand_poke, dex=dex_entry)
