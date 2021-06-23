e00f21f421f23955cc222a84b86715cef7e3cc3b
Alex James <jalex199345@gmail.com>
2021-06-22 20:16:53 -0400

    added search functionality

--- poke_rand.py
+++ poke_rand.py
@@ -1,6 +1,7 @@
 import random
 import requests
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
@@ -15,14 +16,27 @@ def poke_search():
     return render_template('search.html.jinja')
 
 
+@app.route("/search", methods=['POST'])
+def search_handler():
+    searches = {
+        'poke': poke_display
+    }
+    if request.method == 'POST':
+        return searches[request.form['lookupType']](request.form['searchValue'])
+
+
+@app.route("/poke")
+def poke_display(poke: str):
+    poke_basic_info = requests.get(f'https://pokeapi.co/api/v2/pokemon/{poke}').json()
+    poke_dex_info = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{poke}').json()
+    dex_entry = ''
+    for entry in poke_dex_info['flavor_text_entries']:
+        if entry['language']['name'] == 'en':
+            dex_entry = entry['flavor_text']
+    return render_template('poke.html.jinja', nav="search", title="Search", poke=poke_basic_info, dex=dex_entry)
+
+
 @app.route("/random")
 def poke_rand():
     rand_id = str(random.randint(0, 899))
-    rand_poke = requests.get(f'https://pokeapi.co/api/v2/pokemon/{rand_id}').json()
-    rand_poke_info = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{rand_id}/').json()
-    dex_entry = ''
-    for entry in rand_poke_info['flavor_text_entries']:
-        if entry['language']['name'] == 'en':
-            dex_entry = entry['flavor_text']
-            break
-    return render_template('poke.html.jinja', nav="random", title="Random", poke=rand_poke, dex=dex_entry)
+    return poke_display(rand_id)
